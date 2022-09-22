6bb3c05dcfe3638383905c8d9681d20aa0215400
James Bedford <jamesbedford@protonmail.com>
2022-09-22 22:23:13 +1000

    UI updates

--- webAPI/Controllers/DepartmentController.cs
+++ webAPI/Controllers/DepartmentController.cs
@@ -111,5 +111,27 @@ namespace WebAPI.Controllers
 
             return new JsonResult("Deleted Successfully");
         }
+
+        [Route("GetAllDepartmentNames")]
+        [HttpGet]
+        public JsonResult GetAllDepartmentNames()
+        {
+            string query = @"SELECT DepartmentName FROM dbo.department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using SqlCommand myCommand = new SqlCommand(query, myCon);
+                myReader = myCommand.ExecuteReader();
+                table.Load(myReader);
+
+                myReader.Close();
+                myCon.Close();
+            }
+
+            return new JsonResult(table);
+        }
     }
 }
